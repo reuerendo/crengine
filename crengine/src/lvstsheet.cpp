28da72fc1ea8a4bab6b0934d40f3492d24b6d47a
Oleh Tiuriakov <oleh.tiuriakov@gmail.com>
2026-01-16 14:04:08 +0100

    5

--- crengine/src/lvstsheet.cpp
+++ crengine/src/lvstsheet.cpp
@@ -317,12 +317,14 @@ enum LVCssSelectorPseudoElement
 {
     csspe_before = 1,   // ::before
     csspe_after  = 2,   // ::after
+    csspe_first_letter = 3, // ::first-letter
 };
 
 static const char * css_pseudo_elements[] =
 {
     "before",
     "after",
+    "first-letter",
     NULL
 };
 
@@ -6922,7 +6924,8 @@ void LVCssSelector::applyToPseudoElement( const ldomNode * node, css_style_rec_t
     css_style_rec_t * target_style = NULL;
     if ( node->getNodeId() == el_pseudoElem ) {
         if (    ( _pseudo_elem == csspe_before && node->hasAttribute(attr_Before) )
-             || ( _pseudo_elem == csspe_after  && node->hasAttribute(attr_After)  ) ) {
+             || ( _pseudo_elem == csspe_after  && node->hasAttribute(attr_After)  )
+             || ( _pseudo_elem == csspe_first_letter && node->hasAttribute(attr_FirstLetter) ) ) {
             target_style = style;
         }
     }
@@ -6944,6 +6947,12 @@ void LVCssSelector::applyToPseudoElement( const ldomNode * node, css_style_rec_t
                 style->pseudo_elem_after_style = new css_style_rec_t;
             }
             target_style = style->pseudo_elem_after_style;
+        }
+        else if ( _pseudo_elem == csspe_first_letter ) {
+            if ( !style->pseudo_elem_first_letter_style ) {
+                style->pseudo_elem_first_letter_style = new css_style_rec_t;
+            }
+            target_style = style->pseudo_elem_first_letter_style;
         }
     }
 
