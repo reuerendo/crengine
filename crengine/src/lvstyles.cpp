deb186301aea9fabf7644b8a763aec2206d69da9
Oleh Tiuriakov <oleh.tiuriakov@gmail.com>
2026-01-19 08:42:15 +0100

    Update lvstyles.cpp

--- crengine/src/lvstyles.cpp
+++ crengine/src/lvstyles.cpp
@@ -119,7 +119,7 @@ lUInt32 calcHash(css_style_rec_t & rec)
          + (lUInt32)rec.word_break) * 31
          + (lUInt32)rec.box_sizing) * 31
          + (lUInt32)rec.caption_side) * 31
-         + (lUInt32)rec.cr_hint.pack()) * 31
+         + (lUInt32)rec.cr_hint.pack() * 31
          + (lUInt32)rec.font_name.getHash()
          + (lUInt32)rec.background_image.getHash()
          + (lUInt32)rec.content.getHash());
